f38ae5507a4d98d892fe1ea4ab5f5b86260b34af
TylersDurden <srobbin1@ithaca.edu>
2019-05-03 20:09:09 -0400

    updating nlp stuff mostly

--- H4CK/NLP/knowledge.py
+++ H4CK/NLP/knowledge.py
@@ -1,5 +1,6 @@
 import urllib
 import urllib2
+import utils
 import sys
 import os
 
@@ -17,6 +18,23 @@ class MetaAgent:
         fd = os.open(topic+'_result'+".txt",os.O_RDWR)
         ret = os.write(fd,raw_data)
 
+class Knowledge:
+    kb = {}
+    vocabulary = list()
+    def __init__(self):
+        self.initialize()
+
+    def initialize(self):
+        os.system('ls KB/ >> topics.txt')
+        known_subjects = utils.swap('topics.txt',True)
+        for subj in known_subjects:
+            topic = subj.split('_result.txt')[0].replace('_',"")
+            self.kb[topic] = utils.swap('KB/'+subj, False)
+            sz = len(self.kb[topic])
+            print 'Importing Knowledge of subject\t%s\033[1m\t\b%d lines\033[0m' % (topic,sz)
+        vocabulary = utils.swap('words.txt', False)
+        print '\033[1m\033[34m%d Words Added to Vocabulary\033[0m' % (len(vocabulary))
+
 def fetch_resource(url):
     user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
     values = {'name': 'KnowledgeBot',
@@ -31,3 +49,5 @@ def fetch_resource(url):
 
 if 'get' in sys.argv:
     MetaAgent(sys.argv[2])
+else:
+    knowledge_base = Knowledge()
