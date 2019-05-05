ec3370369fdab4cee486df78329b4d51a71a7640
TylersDurden <srobbin1@ithaca.edu>
2019-05-05 14:59:43 -0400

    cleaning

--- H4CK/p2p/sanitizer.py
+++
@@ -1,8 +0,0 @@
-import urllib2
-import sys
-import os
-
-
-actively_malicious_ip_list = 'http://api.cybercure.ai/feed/get_ips'
-actively_malicious_url_list = 'http://api.cybercure.ai/feed/get_url'
-
--- H4CK/p2p/whatsmyip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ifconfig | grep 'netmask 255.255.255.0' | cut -b 14-28
-#EOF
