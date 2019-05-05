9c55b319f88fbc84fafa493c5f5e529448ebc710
TylersDurden <srobbin1@ithaca.edu>
2019-05-05 01:57:59 +0000

    minor

--- Charts/btc.py
+++ Charts/btc.py
@@ -16,21 +16,36 @@ def query(link):
 def parse_btc_query(btc_raw,verbose):
     data = {'date': '',
             'prices': []}
-    for item in btc_raw.split(","):
-        dat = item.split(":")
+    for item in btc_raw.split(','):
+        dat = item.split(':')
         if 'time' in dat[0].replace('}', '').replace('"', ''):
-            date = dat[2].replace('}', '').replace('"', '')
+            date = dat[2].replace('}','').replace('"', '')
             if verbose:
                 print date
             data['date'] = date
-        if '"rate_float"' in dat[0]:
+        if 'rate_float' in dat[0]:
             price = dat[1].replace('}', '')
             if verbose:
                 print '\033[1mPRICE: \033[33m$' + price +'\033[0m'
             data['prices'].append(price)
     return data
 
-log_name = ''
+
+def create_datestr():
+    localtime = time.localtime(time.time())
+    mm = str(localtime.tm_mon)
+    dd = str(localtime.tm_mday)
+    yy = str(localtime.tm_year)
+
+    hh = str(localtime.tm_hour)
+    min = str(localtime.tm_min)
+
+    datestr = 'Prices/market' + mm + yy + dd + '_' + hh + '-' + min + '.txt'
+    return datestr
+
+
+log_name = create_datestr()
+print '\033[1mCreating LOG %s \033[0m' % (log_name)
 db = False
 if not os.path.isdir('Prices/'):
     print 'Making Price Tracking DataBase'
@@ -42,21 +57,22 @@ else:
 t0 = time.time()
 pause = 10
 current_prices = parse_btc_query(query(btc_resource_1), True)
-if not os.path.isfile('Prices/'+log_name):
+if os.path.isfile('Prices/'+log_name):
     print '\t\t** Creating Prices/ Directory For Logging **'
     log_name = 'Prices/' + current_prices['date'].replace(' ', '_') + '.txt'
     print 'touch '+log_name
     os.system('touch '+log_name)
 if db:
-    os.system('echo "'+ str(current_prices['date']) + '\t\tPrice LOG'+'">>'+log_name)
+    os.system('echo + str(current_prices[date]) + ttPrice LOG+>>'+log_name)
     for price in current_prices['prices']:
         os.system('echo '+str(price)+' >> '+log_name)
 dt = 0
-while dt < 3600:
+while dt < 36000:
     current_prices = parse_btc_query(query(btc_resource_1), True)
     for price in current_prices['prices']:
         os.system('echo '+str(price)+' >> '+log_name)
-    os.system('echo "****************************************" >> '+log_name)
+    os.system('echo **************************************** >> '+log_name)
     dt += (time.time()-t0)
     time.sleep(pause)
     print str(dt)+'s Elapsed'
+
